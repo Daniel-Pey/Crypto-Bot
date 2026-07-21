e27232923cf1631ab898a40d10c11f43ad0c603a
Danya <danyaputilin@yandex.ru>
2026-07-21 19:35:56 +0300

    Update __init__.py

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -5,10 +5,12 @@
 from .alert_model import Alert
 from .crypto_models import Coin, UserCoin
 from .user_model import User
+from .sale_model import Sale
 
 __all__ = [
     'Alert',
     'Coin',
     'UserCoin',
-    'User'
+    'User',
+    'Sale'
 ]
